2555530a94bcf9b69e2a122732f7be16c7cb2c3e
patelyashvi3157 <patelyashvi3157@gmail.com>
2026-02-28 17:24:58 +0530

    Add files via upload

---
+++ atm_simulation.py
@@ -0,0 +1,72 @@
+import mysql.connector as m
+try:
+    db=m.connect(host="localhost",user="root",passwd="root")
+    cur=db.cursor()
+    
+    cur.execute("create database if not exists adv_python")
+    cur.execute("use adv_python")
+    #cur.execute("drop table if exists users")
+    cur.execute("create table if not exists users(cname text,mob text,email text,uid varchar(20),pwd text,opbal float)")
+    print("table created")
+
+    while True:
+        print("-----MENU------")
+        print("1.SignUp")
+        print("2.SignIn")
+        print("3.Exit")
+        ch=int(input("Enter Your Choice = "))
+        if ch==1:
+            cname=input("Enter Customer Name = ")
+            mob=input("Enter Mobile Number = ")
+            if len(mob)!=10 or not mob.isdigit():
+                print("Please Enter a Valid Mobile Number")
+                continue
+            email=input("Enter Email ID = ")
+            opbal=input("Enter Opening Balance = ")
+            uid=input("Enter User ID = ")
+            pwd=input("Enter Password = ")
+            cpwd=input("Please Confirm Your Password = ")
+            if pwd!=cpwd:
+                print("Your Password Must Match Your Confirmed Password")
+                continue
+            
+            cur.execute("insert into users values('"+cname+"','"+mob+"','"+email+"','"+uid+"','"+pwd+"','"+opbal+"')")
+            db.commit()
+            print("Registration Done Successfully")
+        elif ch==2:
+            #check uid and pwd and confirm
+            cur.execute("select * from users")
+            for i in cur:
+                print(i)
+            uid=input("Enter User Id = ")
+            pwd=input("Enter Password = ")
+            cur.execute("select * from users where uid='"+uid+"' and pwd='"+pwd+"' ")
+            ans=cur.fetchone()
+            if ans:
+                while True:
+                    print("Welcome User ",ans[0])
+                    print("1.Deposite\n2.Withdraw\n3.Display\n4.Back")
+                    choice=int(input("Enter Your Choice = "))
+                    if choice==1:
+                        amt=int(input("Enter Deposite Amount = "))
+                        cur.execute("update users set opbal=opbal+'"+str(amt)+"' where uid='"+uid+"' ")
+                        db.commit()
+                        print("Amount deposited ")
+                    elif choice==2:
+                        amt=int(input("Enter Withdraw Amount = "))
+                        cur.execute("update users set opbal=opbal-'"+str(amt)+"' where uid='"+uid+"' ")
+                        db.commit()
+                        print("Amount withdrawn ")
+                    elif choice==3:
+                        cur.execute("select * from users where uid='"+uid+"' ")
+                        for i in cur:
+                            print(i)
+                    elif choice==4:
+                        break
+            else:
+                print("Invalid Credentials")
+        elif ch==3:
+            break
+
+except Exception as e:
+    print(e)
